14101457f1bde8927e4012e2fed0c756a1cd9c6e
Jamal alayq <gamalal3yk@gmail.com>
2020-04-09 12:51:00 +0200

    Prepare the package file.

--- Package.swift
+++ Package.swift
@@ -5,24 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "Name",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Name",
-            targets: ["Name"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            type: .dynamic,
+            targets: ["Name"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Name",
-            dependencies: []),
+            path: "Sources"            
+        ),
         .testTarget(
             name: "NameTests",
-            dependencies: ["Name"]),
+            dependencies: ["Name"]
+        ),
     ]
 )
